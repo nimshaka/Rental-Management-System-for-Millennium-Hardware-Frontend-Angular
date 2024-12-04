9cb6f2174acd839e6de8deba2eec33292f73bbec
nimshaka <nimshakaravintha@gmail.com>
2024-12-04 17:22:49 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { HeaderComponent } from './common/header/header.component';
+import { ItemComponent } from './pages/item/item.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,DashboardComponent,HeaderComponent,ItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { CustomerComponent } from './pages/customer/customer.component';
+import { ItemComponent } from './pages/item/item.component';
+import { RentalComponent } from './pages/rental/rental.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component:DashboardComponent
+    },
+    {
+        path:'customer',
+        component:CustomerComponent
+    },
+    {
+        path:'item',
+        component:ItemComponent
+    },
+    {
+        path:'rent',
+        component:RentalComponent
+    }
+];
